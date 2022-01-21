dda1dc1671155880c99f519bd0c33c5eeda92e70
Jhonatan Espinoza <jhonatan.espinoza@seteinfo.com>
2022-01-21 17:57:49 -0500

    Cambio de nombre carpeta build a docs

---
+++ docs/precache-manifest.82af7d80689fcb21ed50dc1d8fa46975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091d3e48a1606aa19f70ac378b7daa5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f7c3ee48b566b39a3c",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "b9f9a9ff4b29fece63e4",
+    "url": "/static/css/main.b24d1436.chunk.css"
+  },
+  {
+    "revision": "74f7c3ee48b566b39a3c",
+    "url": "/static/js/2.1daecf3a.chunk.js"
+  },
+  {
+    "revision": "b9f9a9ff4b29fece63e4",
+    "url": "/static/js/main.de1abaf8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
